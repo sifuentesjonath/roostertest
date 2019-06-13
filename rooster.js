681d77494e12f2ded3b4f9a8ac0a5f469441d433
Jonathan Sifuentes <jonathan@relatibyte.com.mx>
2019-06-13 12:12:37 -0500

    mostrando el nombre y la edad de manera facil

--- rooster.js
+++ rooster.js
@@ -1,10 +1,18 @@
+
+
+var areglo = [];
+
+
 fetch('http://dotnet-rooster.herokuapp.com/')
 .then(function (response) {
   // console.log(response)
   return response.json()
+  
+          
 })
-.then (function (id){
-    console.log("ando jalando")
+.then (function (user){
+       console.log(user[0].name +' '+ user[0].age)
+       
 })
 .catch(function() {
   console.log('algo falló')
